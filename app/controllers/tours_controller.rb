8583f08e2b162552cd21f8d14f6fa4b970b8bf45
David Zwagemaker <david@zwagemaker.nl>
2018-06-04 14:51:27 +0100

    Added basic structure for new/create/show tours

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,2 +1,33 @@
 class ToursController < ApplicationController
+  def new
+    @tour = Tour.new()
+    # tour.user = current_user
+  end
+
+  def create
+    @tour = Tour.new(tour_params)
+    @tour.user = current_user
+
+    respond_to do |format|
+      if @tour.save
+        format.html { redirect_to @tour, notice: 'Tour was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def show
+    render layout: "tour"
+  end
+
+  private
+
+  def tour_params
+    params.require(:tour).permit(:title, :price, :duration, :description)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_131601) do
+ActiveRecord::Schema.define(version: 2018_06_04_132956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_06_04_131601) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["user_id"], name: "index_tours_on_user_id"
   end
 
