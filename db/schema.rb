51e8fa4bce9ca3f69c001b4072c9ebafd6516f48
Alessandra Patricio <allypatricio@gmail.com>
2018-06-04 14:58:40 +0100

    Added buttons & messages scss

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_06_04_113704) do
-
+ActiveRecord::Schema.define(version: 2018_06_04_131601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +56,6 @@ ActiveRecord::Schema.define(version: 2018_06_04_113704) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.index ["user_id"], name: "index_tours_on_user_id"
   end
 
