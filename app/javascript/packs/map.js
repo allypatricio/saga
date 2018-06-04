f9daf1163956836829a6111a3781ec985571e7aa
David Zwagemaker <david@zwagemaker.nl>
2018-06-04 20:42:32 +0100

    Added autocomplete dropdown; turned off markers for time being

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,4 +1,5 @@
 import GMaps from 'gmaps/gmaps.js';
+import { autocomplete } from '../components/autocomplete';
 
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
@@ -14,14 +15,16 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
         position: google.maps.ControlPosition.TOP_RIGHT
     }
     });
-  const markers = JSON.parse(mapElement.dataset.markers);
-  map.addMarkers(markers);
-  if (markers.length === 0) {
-    map.setZoom(2);
-  } else if (markers.length === 1) {
-    map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
-  } else {
-    map.fitLatLngBounds(markers);
-  }
+  // const markers = JSON.parse(mapElement.dataset.markers);
+  // map.addMarkers(markers);
+  // if (markers.length === 0) {
+  //   map.setZoom(2);
+  // } else if (markers.length === 1) {
+  //   map.setCenter(markers[0].lat, markers[0].lng);
+  //   map.setZoom(14);
+  // } else {
+  //   map.fitLatLngBounds(markers);
+  // }
 }
+
+autocomplete();
