50f9f75ea238c25343b432c88518d43d144890b0
Emily Burns <emily5001@gmail.com>
2018-06-05 13:10:11 +0100

    created basic layout of landing page

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_082326) do
+ActiveRecord::Schema.define(version: 2018_06_04_113704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "tour_id"
+    t.integer "status"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.integer "hints_used"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tour_id"], name: "index_bookings_on_tour_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.bigint "tour_id"
+    t.string "address"
+    t.string "photo"
+    t.string "story"
+    t.string "question"
+    t.string "hint"
+    t.string "answer"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tour_id"], name: "index_locations_on_tour_id"
+  end
+
+  create_table "tours", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "category_id"
+    t.string "title"
+    t.integer "price"
+    t.integer "duration"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_tours_on_category_id"
+    t.index ["user_id"], name: "index_tours_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +78,9 @@ ActiveRecord::Schema.define(version: 2018_06_04_082326) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "tours"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "locations", "tours"
+  add_foreign_key "tours", "categories"
+  add_foreign_key "tours", "users"
 end
