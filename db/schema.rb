3b9d03616973350a6b0965f8bf91ffccd97ffbf9
David Zwagemaker <david@zwagemaker.nl>
2018-06-05 08:40:32 +0100

    Show locations list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_135218) do
+ActiveRecord::Schema.define(version: 2018_06_05_072432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2018_06_04_135218) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "title"
     t.index ["tour_id"], name: "index_locations_on_tour_id"
   end
 
