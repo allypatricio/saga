200e68142daf0bd29a6109fe2047dd9b96b56bf6
Swen Kunzel <swenkuenzel@gmail.com>
2018-06-05 11:32:06 +0100

    simpled seeds added

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,9 @@
 class LocationsController < ApplicationController
+
+
+  private
+
+   def location_params
+    params.require(:location).permit(:address, :story, :question, :hint, :answer, :position, :photo, :photo_cache)
+  end
 end
