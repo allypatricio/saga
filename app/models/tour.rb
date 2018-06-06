46bdf3f3cf8e73e515d742e7f9a58a652f874c19
Swen Kunzel <swenkuenzel@gmail.com>
2018-06-06 10:04:50 +0100

    sliders for filters

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -5,7 +5,7 @@ class Tour < ApplicationRecord
   CATEGORIES = ["Bachelor Party", "Team buidling", "Kids", "Rainy day", "Sightseeing"]
 
   scope :category, -> (category) { where category: category }
-  scope :duration, -> (duration) { where("duration > ? AND duration < ?", duration.split(",").first, duration.split(",").last) }
-  scope :price,    -> (price)    { where("price > ? AND price < ?", price.split(",").first, price.split(",").last) }
+  scope :duration, -> (duration) { where("duration < ?", duration) }
+  scope :price,    -> (price)    { where("price < ?", price) }
 
 end
