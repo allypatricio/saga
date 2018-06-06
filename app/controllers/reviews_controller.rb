7795a2498b00a4c185b5d4eab440275df1f2b5f7
Alessandra Patricio <allypatricio@gmail.com>
2018-06-06 14:49:15 +0100

    defined new & create & added validations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,33 @@
 class ReviewsController < ApplicationController
+  before_action :set_booking
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.booking_id = @booking.id
+    if @review.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
