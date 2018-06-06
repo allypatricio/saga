4373752e76d0d4a59a695d2c50b3fbffa072085d
Swen Kunzel <swenkuenzel@gmail.com>
2018-06-06 18:08:35 +0100

    dashboard with bookings

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,13 @@ class DashboardController < ApplicationController
 
   def index
     @tours = Tour.where(user_id: current_user.id)
+    @bookings_filtered = []
+    bookings = Booking.all
+    bookings.each do |booking|
+      if booking.tour.user_id == current_user.id
+        @bookings_filtered << booking
+      end
+    end
 
   end
 end
