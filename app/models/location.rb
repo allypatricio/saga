cf439c6cf86f0dcd36e528e4ec47e86aa4d2b47a
David Zwagemaker <david@zwagemaker.nl>
2018-06-07 11:42:59 +0100

    added validations for lat / lng

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,13 +2,15 @@ class Location < ApplicationRecord
   belongs_to :tour
 
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # before_validation :geocode, if: :will_save_change_to_address?
 
   mount_uploader :photo, PhotoUploader
 
   validates :address, presence: true
   # validates :story, presence: true, length: { in: (20..60) }
   validates :position, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
 
   # TO DO:
   # Uniqueness of position within tour
