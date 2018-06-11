2181039dd4075fe077e670c797f44d2e4c2d9b4b
Swen Kunzel <swenkuenzel@gmail.com>
2018-06-11 14:37:48 +0100

    first version user plays

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
   }
 
   root to: 'home#index'
@@ -22,6 +22,10 @@ Rails.application.routes.draw do
   end
 
   resources :dashboard, only: :index
+
+
+  resources :play, only: [:index, :show]
+
 end
 
 # dashboard
