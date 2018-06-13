6849928799d9695d08315c84c99e4b22360090a4
sknzl <swenkuenzel@gmail.com>
2018-06-13 11:36:47 +0100

    Revert "first version user plays"

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
-    registrations: 'users/registrations',
+    registrations: 'users/registrations'
   }
 
   root to: 'home#index'
@@ -23,10 +23,6 @@ Rails.application.routes.draw do
   end
 
   resources :dashboard, only: :index
-
-
-  resources :play, only: [:index, :show]
-
 end
 
 # dashboard
