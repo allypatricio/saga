bbd2de6c5924e841a5f845b216c2525abf6f60e9
Swen Kunzel <swenkuenzel@gmail.com>
2018-06-14 10:05:16 +0100

    redirects fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,16 +2,20 @@ class HomeController < ApplicationController
 
   def index
     @navbar = false
+    if current_user != nil
        if !current_user.guide
          redirect_to user_landing_page_path
        end
+     end
   end
 
   def user
     @navbar = false
+    if current_user != nil
      if current_user.guide
          redirect_to root_path
       end
+    end
 
   end
 end
