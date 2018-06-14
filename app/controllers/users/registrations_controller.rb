a309c5c0593d394f71c4dac0b483e738c97baeec
Swen Kunzel <swenkuenzel@gmail.com>
2018-06-14 09:47:19 +0100

    manage redirects of landing page and login and signup and update profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -60,6 +60,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
     # super(resource)
   end
 
+  def after_update_path_for(resource)
+    if resource.guide
+      my_tours_path
+    else
+      tours_path
+    end
+  end
+
   #The path used after sign up for inactive accounts.
   def after_inactive_sign_up_path_for(resource)
     super(resource)
